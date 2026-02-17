0a1080d93b06fa49d37ae723207137bdbc546cc5
Ruda Gabriel <rudag3@gmail.com>
2026-02-17 13:19:51 -0400

    locais atualizados

--- gerar_relatorio_do_dia_NORMAL.bat
+++ gerar_relatorio_do_dia_NORMAL.bat
@@ -1,13 +1,25 @@
 @echo off
+
 setlocal
+
 chcp 65001 >nul
+
 for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd"') do set "DATA=%%i"
-set "FDB=C:\Program Files (x86)\SmallSoft\Small Commerce\SMALL.FDB"
+
+set "FDB=%ProgramFiles(x86)%\SmallSoft\Small Commerce\SMALL.FDB"
+
 for /f "tokens=1-3 delims=-" %%a in ("%DATA%") do (set "YYYY=%%a" & set "MM=%%b" & set "DD=%%c")
+
 set "DATA_BR=%DD%/%MM%/%YYYY%"
+
 set "DATA_ARQ=%DD%-%MM%-%YYYY%"
-set "OUT=C:\Users\SERVIDOR\Desktop\relatorio_%DATA_ARQ%_gerencial_por_vendedor.html"
+
+set "OUT=%userprofile%\desktop\relatorio_%DATA_ARQ%_gerencial_por_vendedor.html"
+
 cd /d "%~dp0"
-node "C:\Users\SERVIDOR\Desktop\REL\gerencial_por_vendedor_html_fix_width.js" --fdb "%FDB%" --data "%DATA%" --saida "%OUT%" --user SYSDBA --pass masterkey
+
+node gerencial_por_vendedor_html_fix_width.js --fdb "%FDB%" --data "%DATA%" --saida "%OUT%" --user SYSDBA --pass masterkey
+
 start "" "%OUT%"
+
 exit /b 0
